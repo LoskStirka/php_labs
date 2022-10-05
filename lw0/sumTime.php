66c055dc3a2239fd401872845c13bbf1c7a5f823
LoskStirka <snegoweek12@yandex.ru>
2022-10-05 19:28:17 +0300

    feat (lw0): edited sumTime.php

--- lw0/sumTime.php
+++ lw0/sumTime.php
@@ -1,16 +1,32 @@
 <?php
-$firstTime = '20:20:30';
-$secondTime = '10:20:30';
-$thirdTime = 0;
-function sunTime(string $thirdTime) : string
+$firstString = "";
+$secondString = "";
+$thirdArr[2];
+function calculator(string $firstString, string $secondString) : string
 {
     $correctChars = ' 0123456789:';
-    for ($i = 0; $i < strlen($firstTime); $i++) {
-        if (!strpos($correctChars, $firstTime[$i])) return 'ошибка';
+    for ($i = 0; $i < strlen($firstString); $i++) {
+        if (!strpos($correctChars, $firstString[$i])) return 'ошибка ввода';
     }
-    for ($i = 0; $i < strlen($secondTime); $i++) {
-        if (!strpos($correctChars, $secondTime[$i])) return 'ошибка';
+    for ($i = 0; $i < strlen($secondString); $i++) {
+        if (!strpos($correctChars, $secondString[$i])) return 'ошибка ввода';
     }
-    return $thirdTime;
+    $firstArr = explode(":", $firstString);
+    $secondArr = explode(":", $secondString);
+    for ($i=0; $i<3; ++$i) {
+        $thirdArr[$i] = $firstArr[$i] + $secondArr[$i];
+    }
+    while ($thirdArr[0] >= 24) {
+        $thirdArr[0] = $thirdArr[0]-24;
+    }
+    while ($thirdArr[1] >= 60) {
+        $thirdArr[1] = $thirdArr[1]-60;
+    }
+    while ($thirdArr[2] >= 60) {
+        $thirdArr[2] = $thirdArr[2]-60;
+    }
+    $thirdString = implode(":", $thirdArr);
+    
+    return $thirdString;
 }
-echo sunTime($thirdTime);
+echo calculator('10:20:30','10:20:30');
