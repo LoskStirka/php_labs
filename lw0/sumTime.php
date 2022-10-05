34cc2dc475b1fc707ce0b5e04bd76db82294515b
LoskStirka <snegoweek12@yandex.ru>
2022-10-05 20:39:11 +0300

    feat(lw0): refactored sumTime.php

--- lw0/sumTime.php
+++ lw0/sumTime.php
@@ -13,20 +13,20 @@ function calculator(string $firstString, string $secondString) : string
     }
     $firstArr = explode(":", $firstString);
     $secondArr = explode(":", $secondString);
-    for ($i=0; $i<3; ++$i) {
+    for ($i = 0; $i < 3; ++$i) {
         $thirdArr[$i] = $firstArr[$i] + $secondArr[$i];
     }
     while ($thirdArr[0] >= 24) {
-        $thirdArr[0] = $thirdArr[0]-24;
+        $thirdArr[0] = $thirdArr[0] - 24;
     }
     while ($thirdArr[1] >= 60) {
-        $thirdArr[1] = $thirdArr[1]-60;
+        $thirdArr[1] = $thirdArr[1] - 60;
     }
     while ($thirdArr[2] >= 60) {
-        $thirdArr[2] = $thirdArr[2]-60;
+        $thirdArr[2] = $thirdArr[2] - 60;
     }
     $thirdString = implode(":", $thirdArr);
-    
+
     return $thirdString;
 }
-echo calculator('10:20:30','10:20:30');
+echo calculator('10:20:30', '10:20:30');
